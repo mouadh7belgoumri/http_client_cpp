3f07307947bc296f2ac0879ac84cb943cf7b1e54
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-04-11 19:39:41 +0100

    removing out/ to push again

--- backend/out/build/desktop_app/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mouadh7/work/projects/http_client_cpp/backend
-# Build directory: /home/mouadh7/work/projects/http_client_cpp/backend/out/build/desktop_app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/webview-build")
-subdirs("_deps/json-build")
