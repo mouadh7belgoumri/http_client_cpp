d5b8303dd0d723400131532979d87c659745ec12
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-04-12 11:09:08 +0100

    extending the project backend with vcpkg package manager and integrating the SQLiteCpp package for database interactions to store requests locally

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -3,15 +3,21 @@
 #include "../lib/index_html.h"
 #include <nlohmann/json.hpp>
 #include <thread>
+#include <SQLiteCpp/SQLiteCpp.h>
+
+
 
 using json = nlohmann::json;
 
 int main(int, char**){
     webview::webview w(false, nullptr);
     w.set_title("http_client_cpp");
-    w.set_size(800, 600, WEBVIEW_HINT_NONE);
+    w.set_size(1200, 900, WEBVIEW_HINT_NONE);
     w.set_html( html);
     w.run();
+    
+
+    
     return 0;
 }
     
