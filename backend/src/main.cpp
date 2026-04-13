6340a786e6acadc3172bfc663cc31b62b31fc079
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-04-13 11:34:48 +0100

    included cpp-httplib library for http functionality and ran some tests

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "../lib/httplib.h"
 #include <webview/webview.h>
 #include "../lib/index_html.h"
 #include <nlohmann/json.hpp>
@@ -6,19 +7,37 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 
 
+void say_hello()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
+void spinup_srv()
+{
+    httplib::Server svr;
+
+    svr.Get("/hello", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    std::cout << "Server is running on http://0.0.0.0:8080" << std::endl;
+    std::cout << "Press Ctrl+C to stop the server." << std::endl;
+    std::cout << "this server runs on a seperate thread, so the webview can run concurrently" << std::endl;
+    svr.listen("0.0.0.0", 8080);   
+}
 
 using json = nlohmann::json;
 
-int main(int, char**){
+int main(int, char **)
+{
+
+    std::thread t(say_hello);
+    std::thread lsrv(spinup_srv);
     webview::webview w(false, nullptr);
     w.set_title("http_client_cpp");
     w.set_size(1200, 900, WEBVIEW_HINT_NONE);
-    w.set_html( html);
+    w.set_html(html);
     w.run();
-    
-
-    
+    t.join();
+    lsrv.join();
     return 0;
 }
-    
-
