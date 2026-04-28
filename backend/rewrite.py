6da47126429781af974ff9c8b6ef1e035615fd41
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-04-28 18:53:02 +0100

    finally compiled on windows

---
+++ backend/rewrite.py
@@ -0,0 +1,9 @@
+import sys
+content = open('c:/Users/mouadh/work/projects/http_client_cpp/backend/lib/index_html.h', 'r', encoding='utf-8').read()
+if content.startswith('const char* html = R\"('):
+    content = content[len('const char* html = R\"('):-3] # assumes ')\";'
+    bytes_arr = content.encode('utf-8')
+    with open('c:/Users/mouadh/work/projects/http_client_cpp/backend/lib/index_html.h', 'w', encoding='utf-8') as f:
+        f.write('const char html[] = {')
+        f.write(', '.join(str(b) for b in bytes_arr) + ', 0')
+        f.write('};\n')
