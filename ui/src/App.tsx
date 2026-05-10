3876fa9e3ea4138111e8a5e301925fbe1c619ce6
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-05-10 11:46:06 +0100

    starting to define interface for window object to improve codebase modularity

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
     } catch {
       isSyntaxValid = false;
     }
-  }
+  };
 
   const handleEditorWillMount = (monaco: any) => {
     monaco.editor.defineTheme('custom-dark', {
