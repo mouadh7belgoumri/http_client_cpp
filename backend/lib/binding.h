0cbdddd2f8e42356ab1f28ceded3ed3d3894a69f
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-05-13 14:13:43 +0100

    removed unnecessary complexities, bindings will be decalred and passed just as function pointers

--- backend/lib/binding.h
+++ backend/lib/binding.h
@@ -1,27 +1,7 @@
 #ifndef BINDING_H
 #define BINDING_H
 #include <webview/webview.h>
-class binding
-{
-private:
-    std::shared_ptr<webview::webview> m_window;
-public:
-    binding() =delete;
-    binding(const binding&) =delete;
-    binding(binding&&) =delete;
-    binding(std::shared_ptr<webview::webview> w);
-    ~binding(){};
-    binding operator=(const binding&) =delete;
-    binding operator=(binding&&) =delete;
-    binding operator()(std::string, std::string, void*);
-};
 
-class getRequests : public binding{
-    getRequests() =delete;
-    getRequests(const getRequests&) =delete;
-    getRequests(getRequests&&) =delete;
-    getRequests(std::shared_ptr<webview::webview>);
-
-};
+void getRequests(std::string, std::string, void*);
 
 #endif
