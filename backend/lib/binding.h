378bb8b819ddbab6de7bd07c4e8ad769bda1a84d
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-05-14 16:33:05 +0100

    retook the funciton objects approach for better type safety and encapsulation

--- backend/lib/binding.h
+++ backend/lib/binding.h
@@ -2,6 +2,23 @@
 #define BINDING_H
 #include <webview/webview.h>
 
-void getRequests(std::string, std::string, void*);
+class binding {
+    private:
+        std::shared_ptr<webview::webview> m_window;
+
+    public:
+        binding() =delete;
+        binding(const binding&) =delete;
+        binding(binding&&) =delete;
+        binding(std::shared_ptr<webview::webview>);
+        ~binding() =default;
+        binding operator=(const binding&) =delete;
+        binding operator=(binding&&) =delete;
+        void operator()(std::string, std::string, void*); 
+};
+
+class getRequests : public binding{
+    
+};
 
 #endif
