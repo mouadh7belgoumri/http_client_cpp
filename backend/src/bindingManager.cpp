b38aefe6e372da7de2912fdebd88ee6d578b7436
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-05-22 14:17:05 +0100

    capturing a pointer(this) of the binding manager instance in order to access the member variables inside the lambdas, still didn't work

--- backend/src/bindingManager.cpp
+++ backend/src/bindingManager.cpp
@@ -27,58 +27,21 @@ void bindingManager::getRequestsWorker(const std::string &id, const std::string 
                 i++;
             }
             std::lock_guard w_lck(m_window_mutex);
-            m_window->dispatch([m_window, id, j]()
-                               { m_window->resolve(id, 0, j.dump()); });
+            m_window->dispatch([this, id, j]()
+                               { this->m_window->resolve(id, 0, j.dump()); });
         }
         catch (const std::exception &e)
         {
             std::cerr << e.what() << '\n';
             std::cout << "Error retrieving requests from database." << std::endl;
-            std::lock_guard w_lck(w_mutex_copy);
-            m_window->dispatch([id, e, m_window]()
-                               { m_window->resolve(id, 1, std::string(e.what())); });
+            std::lock_guard w_lck(m_window_mutex);
+            m_window->dispatch([this, id, e ]()
+                               { this->m_window->resolve(id, 1, std::string(e.what())); });
         }
     }
 }
 void bindingManager::getRequests(const std::string &id, const std::string &req, void *arg)
 {
-    std::thread([id, req]()
-                {
-                             try
-                             {
-                                 
-                                 std::lock_guard<std::mutex> db_lock(m_db_mutex);
-                                 SQLite::Database db{"requests.db", SQLite::OPEN_READONLY};
-                                 json j = json::array();
-                                 SQLite::Statement query{db, "SELECT id, method, path, headers, body, stored FROM requests"};
-                                 int i = 0;
-                                 while (query.executeStep())
-                                 {
-                                     json req_json;
-                                     req_json["id"] = query.getColumn(0).getString();
-                                     req_json["method"] = query.getColumn(1).getString();
-                                     req_json["path"] = query.getColumn(2).getString();
-                                     req_json["headers"] = json::parse(query.getColumn(3).getString());
-                                     req_json["body"] = json::parse(query.getColumn(4).getString());
-                                     req_json["stored"] = json::parse(query.getColumn(5).getString());
-                                     j.push_back(req_json);
-                                     i++;
-                                 }                                 
-                                 std::lock_guard w_lck(w_mutex_copy);
-                                 m_window->dispatch([m_window, id, j]()
-                                 {
-                                     m_window->resolve(id, 0, j.dump());
-                                 });
-                             }
-                             catch (const std::exception& e)
-                             {
-                                std::cerr << e.what() << '\n';
-                                std::cout << "Error retrieving requests from database." << std::endl;
-                                std::lock_guard w_lck(w_mutex_copy);
-                                m_window->dispatch([id, e, m_window]()
-                                 {
-                                     m_window->resolve(id, 1, std::string(e.what()));
-                                 });
-                             } })
-        .detach();
+    std::thread t{this->getRequestsWorker,ref(id)};
+    t.detach();
 }
